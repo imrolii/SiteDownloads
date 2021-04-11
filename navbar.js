3a728da387f106aef20607a3bf1ab97183a63ce6
Oliver <79724190+imrolii@users.noreply.github.com>
2021-04-11 12:34:40 +0100

    Create navbar.js

---
+++ navbar.js
@@ -0,0 +1,9 @@
+function OpenYoutube() {
+window.location.href = "https://totallypikachu.tk/youtube/";
+}
+function OpenDownloads() {
+window.location.href = "http://imrolii-html.tk/downloads";
+}
+function StandardSite() {
+window.location.href = "https://www.imrolii.tk/";
+}
